e523602b97f0ea2a25ef512f42c7cc0b51e1b2dd
deepak1051 <deepakpanwar1051@gmail.com>
2022-11-30 00:48:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import Main from "./components/Main"
+
+import SingleUser from "./components/SingleUser"
+import './app.css';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header hello='header' />
+
+      <Routes>
+        <Route element={<Main />} path="/" />
+        {/* <Route element={<Repos />} path="/repos/:user" /> */}
+        <Route element={<SingleUser />} path="/:user" />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
